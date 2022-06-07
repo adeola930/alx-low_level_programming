06942fc3c88545644225a905e53d0af6599c4bdd
adeola930 <isarah930@gmail.com>
2022-06-07 15:14:03 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,7 +4,7 @@
 /**
  * main - main block
  * Description: Find and print the first 98 fib numbers starting with 1 and 2.
- * Numbers should be coma and space separated.
+ * Numbers should be coma and space separated
  * Return: 0
  */
 int main(void)
@@ -16,22 +16,24 @@ int main(void)
 	printf("%lu, %lu, ", bk1, bk2);
 	for (count = 2; count < 98; count++)
 	{
-		hold1 = (bk1 + bk2) / LARGEST;
-		hold2 = (bk1 + bk2) % LARGEST;
-		hold3 = fr1 + fr2 + hold1;
-		fr1 = fr2, fr2 = hold3;
-		bk1 = bk2, bk2 = hold2;
-		printf("%lu%010lu", fr2, bk2);
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
 	}
-	else
-	{
-		hold2 = bk1 + bk2;
-		bk1 = bk2, bk2 = hold2;
-		printf("%lu", bk2);
-	}
-	if (count != 97)
-		printf(", ");
-}
-printf("\n");
-return (0);
+	printf("\n");
+	return (0);
 }
